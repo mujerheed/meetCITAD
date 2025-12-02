69d8b8b279934d9b8a25f6200547fa02ad4fb429
Mujerheed <mujerheed20@gmail.com>
2025-12-02 15:18:52 +0530

    Backend: Add models index for centralized export
    - Export all 7 MongoDB models from single file
    - Simplifies imports across the application
    - Models: User, Admin, Event, Certificate, CertificateTemplate, Feedback, Notification

---
+++ backend/src/models/index.js
@@ -0,0 +1,12 @@
+/**
+ * MongoDB Models Export
+ * Centralized export for all Mongoose models
+ */
+
+export { default as User } from './User.js';
+export { default as Admin } from './Admin.js';
+export { default as Event } from './Event.js';
+export { default as Certificate } from './Certificate.js';
+export { default as CertificateTemplate } from './CertificateTemplate.js';
+export { default as Feedback } from './Feedback.js';
+export { default as Notification } from './Notification.js';
